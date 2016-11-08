bb146bf35585d466d8c3aa722d61bc6a1976726c
andregri <andrea.grillo96@live.com>
2016-11-08 22:28:53 +0100

    Update if_complex.asm

--- if_complex.asm
+++ if_complex.asm
@@ -4,11 +4,11 @@ if(a == b && a > 2) {
 
 ########################################################
 
-seq $t0, a, b		# set $t0 a 1 se a == b, altrimenti a 0
+seq 	$t0, a, b		# set $t0 a 1 se a == b, altrimenti a 0
 sgt	$t1, a, 2		# set $t1 a 1 se a > 2, altrimenti a 0
-and $t2, $t0, $t1	# $t2 = $t0 & $t1
+and 	$t2, $t0, $t1		# $t2 = $t0 & $t1
 
-bne $t2, 1, exit	# se $t2 != 1 (la condizione è falsa), esci
+bne 	$t2, 1, exit		# se $t2 != 1 (la condizione è falsa), esci
 #codice...
 
 exit:
