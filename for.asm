f7534f42cd1607eb19f866d393f31157d05009eb
andregri <andrea.grillo96@live.com>
2016-11-08 22:27:41 +0100

    Update for.asm

--- for.asm
+++ for.asm
@@ -1,32 +1,48 @@
-# for(int i = 0; i < 20; i++) {
-# 	codice;
-# }
+#########################################################################
+# c
+#########################################################################
+for(int i = 0; i < 20; i++) {
+	codice;
+}
 
-	li 		$t0, 0				# i = 0 one time initialization
-	li 		$t1, 20				# carico la costante per fare il confronto
+#########################################################################
+# asm
+#########################################################################
+
+	li 		$t0, 0			# i = 0 one time initialization
+	li 		$t1, 20			# carico la costante per fare il confronto
 
 loop1:
 
-	#	qui in mezzo c'è il codice dentro il for
-	#	...
+	# qui in mezzo c'è il codice dentro il for
+	# ...
 	#	
 
-	addi	$t0, $t0, 1			# i++
+	addi		$t0, $t0, 1		# i++
 	blt		$t0, $t1, loop1		# se t0 < t1 salta a loop1
 	
 	
 ###########################################################################
 
-# for(int i = 20; i >= 1; i--) {
-# 	codice;
-# }
+#########################################################################
+# c
+#########################################################################
 
-	li		$t0, 20				# i = 20 one time initialization
-	li		$t1, 1				# limite del loop
+for(int i = 20; i >= 1; i--) {
+	codice;
+}
+
+#########################################################################
+# asm
+#########################################################################
+
+	li		$t0, 20			# i = 20 one time initialization
+	li		$t1, 1			# limite del loop
 	
 loop2:
 
 	#codice
+	#...
 	
-	subu	$t0, $t0, 1			# i--
+	subu		$t0, $t0, 1		# i--
 	bge		$t0, $t1, loop2		# se t0 >= t1 salta a loop2
